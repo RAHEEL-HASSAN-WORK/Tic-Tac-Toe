e7c71473e7aa4e1855e2869601a4e764d597805c
RAHEEL HASSAN <raheelhassanwork@gmail.com>
2025-07-30 12:34:18 +0500

    Update app.js

--- app.js
+++ app.js
@@ -12,16 +12,12 @@ buttons = []
 move_count = 0
 
 
-const winPatterns = [
-  [0, 1, 2],
-  [0, 3, 6],
-  [0, 4, 8],
-  [1, 4, 7],
-  [2, 5, 8],
-  [2, 4, 6],
-  [3, 4, 5],
-  [6, 7, 8],
-];
+# Winning patterns (same as JS)
+win_patterns = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],  # rows
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],  # columns
+    [0, 4, 8], [2, 4, 6]              # diagonals
+]
 
 const resetGame = () => {
   turnO = true;
